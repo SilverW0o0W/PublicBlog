28326be6d41e0438ee82ba19251ef058238df1ab
Silver <silver.codingcat@gmail.com>
2017-03-21 21:02:24 +0800

    delete build

--- build/classes/RtfFile.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `publicblog`.`rtffile` (
-  `RtfId` INT NOT NULL AUTO_INCREMENT,
-  `RtfName` VARCHAR(45) NOT NULL DEFAULT 'default',
-  `RtfPath` VARCHAR(256) NOT NULL DEFAULT 'default',
-  `RtfCreator` VARCHAR(45) NOT NULL DEFAULT 'default',
-  `RtfCreateTime` DATETIME NOT NULL DEFAULT now(),
-  PRIMARY KEY (`RtfId`));
